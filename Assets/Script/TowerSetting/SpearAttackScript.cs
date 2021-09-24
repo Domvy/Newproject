dead7400596ee6b65d0cafe934187fce9f3c749b
Domvy <90752171+Domvy@users.noreply.github.com>
2021-09-24 10:50:43 +0900

    20210924-10:50

--- Assets/Script/TowerSetting/SpearAttackScript.cs
+++ Assets/Script/TowerSetting/SpearAttackScript.cs
@@ -8,15 +8,19 @@ public class SpearAttackScript : MonoBehaviour
 
     private float towerRange; //타워 사거리
     public int towerRangeX = 20; // 사거리 설정값(곱해주는 값)
-    private GameObject target; // 공격 타겟
+    private GameObject target; // 공격 타겟    
 
     public int nowenemyCount = 0;// 총 생산횟수
 
     public int NormalDamage = 0; // 공격력
     public int ArmorPearce = 1; // 방어무시 공격력
 
-    public float timer = 0.0f;
-    public float waitingTime = 1;
+    private float timer = 0.0f;
+    private float waitingTime = 5;
+    private float attackTimer = 0.0f;
+    private float attackDelay = 0.1f;
+
+    LineRenderer lineRenderer;
 
     LayerMask layermask;
 
@@ -26,12 +30,17 @@ public class SpearAttackScript : MonoBehaviour
         enemySpawnList = new List<GameObject>(); // 배열 선언  
         towerRange = new Vector3(1, 1, 1).magnitude * towerRangeX; // 타워 사거리 계산    
         layermask = LayerMask.GetMask("Enemy");
+
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.SetColors(Color.red, Color.red);
+        lineRenderer.SetWidth(0.5f, 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+        attackTimer += Time.deltaTime;
 
         enemySpawnList = GameObject.Find("Controller").GetComponent<EnemySpawn>().enemyList; // 적 생성 배열값 받아옴
         nowenemyCount = GameObject.Find("Controller").GetComponent<EnemySpawn>().nowEnemyCount; // 현재 적 숫자 받아오기        
@@ -39,11 +48,19 @@ public class SpearAttackScript : MonoBehaviour
         {
             Distance(); // 공격함수 실행
         }
-        if (timer > waitingTime)
+        if (attackTimer > attackDelay)
         {
             RayzerAttack();
-            timer = 0.0f;
+            attackTimer = 0;
         }
+
+        if (timer > waitingTime)
+        {
+            Destroy(gameObject);
+        }
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, target.transform.position);
     }
 
     void Distance() //사거리 내의 오브젝트를 찾고 타겟설정
@@ -59,18 +76,19 @@ public class SpearAttackScript : MonoBehaviour
     }
 
     void RayzerAttack()
-    {
-        Vector3 dir = target.transform.position - transform.position;
+    {        
         RaycastHit hit;
+        Vector3 dir = target.transform.position - transform.position;
         if (Physics.Raycast(transform.position, dir, out hit, layermask))
         {
             if(hit.collider.gameObject.tag == "Enemy")
-            hit.collider.gameObject.GetComponent<NormalEnemy>().Hit(NormalDamage, ArmorPearce);
+                hit.collider.gameObject.GetComponent<NormalEnemy>().Hit(NormalDamage, ArmorPearce);
             else if (hit.collider.gameObject.tag == "SpeedEnemy")
                 hit.collider.gameObject.GetComponent<SpeedEnemy>().Hit(NormalDamage, ArmorPearce);
             else if (hit.collider.gameObject.tag == "BigEnemy")
                 hit.collider.gameObject.GetComponent<BigEnemy>().Hit(NormalDamage, ArmorPearce);
         }
+        Debug.DrawRay(transform.position, dir, Color.red);
     }
 }
 
